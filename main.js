cbf04060945471f9842c9426da4d67d2bfec3660
oumaima ait said <oumaimabellanova@gmail.com>
2024-11-27 20:22:09 +0100

    adding regix validation form addPlayer by js

--- main.js
+++ main.js
@@ -12,6 +12,59 @@ fetch('players.json')
 document.getElementById("addPlayerForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
+  //validate for with regix
+
+
+  const regexname = /^[A-Za-z\s]+$/;
+  const regexurl = /^https?:\/\/.*$/;
+
+  if (!regexname.test(name)) {
+    alert("Player name can only contain letters and spaces.");
+    return;
+  }
+
+  if (!regexname.test(nationality)) {
+    alert("Nationality can only contain letters and spaces.");
+    return;
+  }
+
+  if (!regexurl.test(photo)) {
+    alert("Please enter a valid photo URL.");
+    return;
+  }
+
+  if (!regexurl.test(logo)) {
+    alert("Please enter a valid logo URL.");
+    return;
+  }
+
+  if (!regexurl.test(flag)) {
+    alert("Please enter a valid flag URL.");
+    return;
+  }
+
+  if (pace < 0 || pace > 100) {
+    alert("Pace must be between 0 and 100.");
+    return;
+  }
+
+  if (shooting < 0 || shooting > 100) {
+    alert("Shooting must be between 0 and 100.");
+    return;
+  }
+
+  if (dribbling < 0 || dribbling >100){
+    alert("dribbling must be between 0 and 100.")
+  }
+
+  if (defending < 0 || defending >100){
+    alert("defending must be between 0 and 100.")
+  }
+
+  if (physical < 0 || physical >100){
+    alert("physical must be between 0 and 100.")
+  }
+
   const newPlayer = {
     name: document.getElementById("name").value,
     photo: document.getElementById("photo").value,
